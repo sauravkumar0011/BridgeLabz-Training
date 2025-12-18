32dbe2b203a3a000a928104270aa0c5342791f0c
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-19 01:06:45 +0530

    [saurav]: Added AverageMarks program

---
+++ gcr-codebase/programming-elements/Level-1/AverageMarks.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class AverageMarks{
+
+  public static void main(String[] args)
+ {
+
+//Calcuting average marks in PCM
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+    double average = (maths + physics + chemistry) / 3;
+
+  //Printing average mark
+ System.out.println("Sam average mark in PCM is " + average + "%");
+ }
+
+}
