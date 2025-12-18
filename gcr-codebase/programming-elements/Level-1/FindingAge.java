b03015415ee69deed9a8f330227f78412d237fc8
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-19 01:15:31 +0530

    [saurav]: Added FindingAge program

---
+++ gcr-codebase/programming-elements/Level-1/FindingAge.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class FindingAge{
+
+  public static void main(String[] args)
+ {
+  
+  //Calculating age of harry
+  int birthYear = 2000;
+  int currentYear = 2024;
+  int harryAge = currentYear - birthYear;
+
+//printing output
+ System.out.println(" Harry's age in 2024 is : " + harryAge);
+}
+
+}
