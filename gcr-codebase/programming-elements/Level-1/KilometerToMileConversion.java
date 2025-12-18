d6b4384971b2c07087cec284cc2534b54663823c
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-19 01:17:34 +0530

    [saurav]: Added KilometerToMileConversion program

---
+++ gcr-codebase/programming-elements/Level-1/KilometerToMileConversion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class KilometerToMileConversion{
+
+  public static void main(String[] args)
+ {
+
+//Calcuting kilometer to meter
+    double kilometer = 10.8;
+    double result = kilometer * 1.6;
+
+  //Printing result
+  System.out.println("The distance 10.8 km in miles is : " + result);
+ }
+
+}
