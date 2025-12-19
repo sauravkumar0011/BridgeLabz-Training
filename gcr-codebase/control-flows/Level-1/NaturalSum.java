0fc18a827909b472d1e83a50a171eb67eb99d96f
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-19 12:04:08 +0530

    [saurav] : Added NaturalSum program

---
+++ gcr-codebase/control-flows/Level-1/NaturalSum.java
@@ -0,0 +1,43 @@
+//Creating Class with name NaturalSum indicating the purpose is to find and check sum of natural number 
+import java.util.Scanner;
+
+class NaturalSum {
+          public static void main(String[] args){
+              //Create a Scanner Object
+              Scanner input = new Scanner(System.in);
+          
+              //Taking a number as input
+              System.out.print("Enter a number: ");
+              double number = input.nextDouble(); 
+
+              //Find sum of natural number using while loop
+              double sum=0;
+              if(number>0)
+              {
+                for(int i = 1; i <= number; i++)
+                {
+                 sum += i;
+                 }
+              }
+
+             //Find sum of natural number using formula
+             double sumUsingFormula=0.0;
+             if(number >= 0) 
+             {
+               sumUsingFormula =  (number*(number+1)/2);
+             }
+
+             //Compare the two results and print the result
+              if(number == sumUsingFormula)
+              {
+               System.out.println("The sum of natural number using while loop is : " + number);
+               System.out.println("The sum of natural number using formula is: " + sumUsingFormula);
+               }
+             else
+             {
+              System.out.println("The number " + number + "  is not a natural number ");
+             }
+    
+             input.close();
+           }
+}
