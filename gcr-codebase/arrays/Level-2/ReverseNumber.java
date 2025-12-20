0598562b260d4c1cb9077a323ed8b71bed29469a
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-20 11:32:17 +0530

    [saurav] : Added ReverseNumber program

---
+++ gcr-codebase/arrays/Level-2/ReverseNumber.java
@@ -0,0 +1,42 @@
+// Creating a class named ReverseNumber purpose ot reverse a number 
+import java.util.Scanner;
+
+class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Taking a number as input
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        //count number of digits using while loop
+        int originalNumber = number;
+        int count = 0;
+        while (originalNumber != 0) {
+            originalNumber = originalNumber / 10;
+            count++;
+        }
+
+        //create array to store each digits 
+        int[] digits = new int[count];
+        originalNumber = number;
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = originalNumber % 10;
+            originalNumber = originalNumber / 10;
+        }
+
+        //create array to store and reverse the digits using for-loop
+        int[] reverseDigits = new int[count];
+        for (int i = 0; i < count; i++) {
+            reverseDigits[i] = digits[count - 1 - i];
+        }
+
+        System.out.print("Reversed number: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(reverseDigits[i]);
+        }
+
+        input.close();
+    }
+}
