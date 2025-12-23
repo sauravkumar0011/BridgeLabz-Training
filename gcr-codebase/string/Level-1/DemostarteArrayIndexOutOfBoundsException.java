1e617f08ac0e8ac196e84d6e2b17baa368849d24
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-23 11:08:39 +0530

    [saurav] : Added DemostarteArrayIndexOutOfBoundsException program

---
+++ gcr-codebase/string/Level-1/DemostarteArrayIndexOutOfBoundsException.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class DemonstrateArrayIndexOutOfBoundsException
+{
+     public void generateException(String[] names)
+    {
+          System.out.println(names[10]);
+    }
+
+    public void demonstrateException(String[] names)
+    {
+        try {
+            System.out.println(names[10]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException is : " + e.getMessage());
+        }
+        catch (RuntimeException e) {
+            System.out.println("Generic RuntimeException : " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        // Take size of array
+        System.out.print("Enter number of names: ");
+        int size = sc.nextInt();
+        sc.nextLine();
+
+
+        String[] names = new String[size];
+
+        // Take user input
+        System.out.println("Enter names:");
+        for(int i = 0; i < size; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        DemonstrateArrayIndexOutOfBoundsException obj = new DemonstrateArrayIndexOutOfBoundsException();
+
+        try {
+            obj.generateException(names);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+             obj.demonstrateException(names);
+            System.out.println("ArrayIndexOutOfBoundsException is : " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
