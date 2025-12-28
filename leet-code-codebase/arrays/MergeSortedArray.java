dd23b246e9b6f09a64593edb9be9445d2efb2b33
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-28 19:44:23 +0530

    [Saurav] : Added MergeSortedArray program

---
+++ leet-code-codebase/arrays/MergeSortedArray.java
@@ -0,0 +1,35 @@
+class MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;          // pointer for nums1
+        int j = n - 1;          // pointer for nums2
+        int k = m + n - 1;      // pointer for merged array
+
+        // Merge from the end
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // Copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
