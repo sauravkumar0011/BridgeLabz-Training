9a42ba59e760ecd75296df711aedadcf3f502926
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-29 00:01:55 +0530

    [Saurav] : Added MaxConsecutiveOnes program

---
+++ leet-code-codebase/arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+class MaxConsecutiveOnes {
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+}
