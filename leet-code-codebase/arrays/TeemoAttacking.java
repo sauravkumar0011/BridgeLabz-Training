9f041edcd82a768eec5ec8a15902c10dda034d85
sauravkumar0011 <studentlife9341@gmail.com>
2025-12-29 11:18:33 +0530

    [saurav] : Added TeemoAttacking program

---
+++ leet-code-codebase/arrays/TeemoAttacking.java
@@ -0,0 +1,22 @@
+class TeemoAttacking {
+
+    public static int findPoisonedDuration(int[] timeSeries, int duration) {
+        if (timeSeries.length == 0 || duration == 0) return 0;
+
+        int total = 0;
+
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            total += Math.min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+
+        total += duration;
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] timeSeries = {1, 2};
+        int duration = 2;
+
+        System.out.println(findPoisonedDuration(timeSeries, duration));
+    }
+}
