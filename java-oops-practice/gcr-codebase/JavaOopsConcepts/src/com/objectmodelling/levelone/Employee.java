87860dcd1a6f7b5e5b618dc7891d7ccceda6fb18
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-01 11:55:19 +0530

    [saurav] : Added Employee program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/levelone/Employee.java
@@ -0,0 +1,10 @@
+package com.objectmodelling.levelone;
+
+public class Employee {
+
+	 String employeeName;
+
+	    public Employee(String employeeName) {
+	        this.employeeName = employeeName;
+	    }
+}
