0b778b5fd3c91f2c8b31b1beabfc375f5acba23f
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-01 11:57:59 +0530

    [saurav] : Added Product program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/levelone/Product.java
@@ -0,0 +1,13 @@
+package com.objectmodelling.levelone;
+
+public class Product {
+
+	 String name;
+	    double price;
+
+	    public Product(String name, double price) {
+	        this.name = name;
+	        this.price = price;
+	    }
+	}
+
