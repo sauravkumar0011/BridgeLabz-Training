94b8fad7e57d28ccf17dd1c27a2a7b2fb3740488
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-01 11:53:53 +0530

    [saurav] : Added Department1 program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/levelone/Department1.java
@@ -0,0 +1,10 @@
+package com.objectmodelling.levelone;
+
+public class Department1 {
+
+	String departmentName;
+
+    public Department1(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
