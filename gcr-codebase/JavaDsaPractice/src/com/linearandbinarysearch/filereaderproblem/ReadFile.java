c52425e688cd98d39136d0991a19ec28c122fed8
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-08 00:16:48 +0530

    [saurav] : Added filereaderproblem program

---
+++ gcr-codebase/JavaDsaPractice/src/com/linearandbinarysearch/filereaderproblem/ReadFile.java
@@ -0,0 +1,23 @@
+package com.linearandbinarysearch.filereaderproblem;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("input.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
