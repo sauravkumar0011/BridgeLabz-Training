48a2f50bb9f8ef088f0c1e9f8d75e0d281a7e664
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-09 11:48:17 +0530

    [saurav] : Added tourmate app program

---
+++ java-oops-practice/scenario-based/scenario-based-problems/src/com/tourmate/Activity.java
@@ -0,0 +1,16 @@
+package com.tourmate;
+
+public class Activity {
+
+	private String activityName;
+    private double cost;
+
+    public Activity(String activityName, double cost) {
+        this.activityName = activityName;
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
