d69eb3d02fb3c93b03494f04f04807a0067c37c1
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-10 11:15:02 +0530

    [saurav] : Added foodloop program

---
+++ java-oops-practice/scenario-based/scenario-based-problems/src/com/foodloop/IOrderable.java
@@ -0,0 +1,7 @@
+package com.foodloop;
+
+public interface IOrderable {
+
+	void placeOrder();
+	void cancelOrder();
+}
---
+++ java-oops-practice/scenario-based/scenario-based-problems/src/com/foodloop/NonVegItem.java
@@ -0,0 +1,8 @@
+package com.foodloop;
+
+public class NonVegItem extends FoodItem{
+
+	public NonVegItem(String name, double price, boolean availability, int stock) {
+		super(name, "Non-Veg item", price, true, stock);
+	}
+}
