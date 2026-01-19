71e928cb91560aa25b4160599547aa589aef1175
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-20 00:14:55 +0530

    [saurav] : Added fitness tracker program

---
+++ scenario-based/scenario-based-problems/src/com/fitnesstracker/FitnessTrackerMain.java
@@ -0,0 +1,21 @@
+package com.fitnesstracker;
+
+public class FitnessTrackerMain {
+
+    public static void main(String[] args) {
+
+        StepBoard board = new StepBoard();
+
+        board.addSteps(5000);
+        board.addSteps(12000);
+        board.addSteps(8000);
+        board.addSteps(15000);
+        board.addSteps(9500);
+
+        board.sortSteps();
+
+        System.out.println("=== Daily Step Leaderboard ===\n");
+        board.displaySteps();
+    }
+}
+
