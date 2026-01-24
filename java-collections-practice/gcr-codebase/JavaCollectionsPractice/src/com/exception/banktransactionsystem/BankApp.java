0759b7320a18526547f861177f41e6d6c56a9649
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-25 00:30:29 +0530

    [saurav] : Added bank transaction system program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/banktransactionsystem/BankApp.java
@@ -0,0 +1,18 @@
+package com.exception.banktransactionsystem;
+
+public class BankApp {
+
+	public static void main(String[] args) {
+
+        BankAccount account = new BankAccount(5000);
+
+        try {
+            account.withdraw(6000);  
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+	}
+
+}
