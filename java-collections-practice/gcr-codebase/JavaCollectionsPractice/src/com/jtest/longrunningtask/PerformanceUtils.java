a8db16832fea95b1b33736c01dc13749268fc2a2
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-25 19:41:20 +0530

    [saurav] : Added long running task program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/jtest/longrunningtask/PerformanceUtils.java
@@ -0,0 +1,13 @@
+package com.jtest.longrunningtask;
+
+public class PerformanceUtils {
+
+	  public static String longRunningTask() {
+	        try {
+	            Thread.sleep(3000); 
+	        } catch (InterruptedException e) {
+	            Thread.currentThread().interrupt();
+	        }
+	        return "Task Completed";
+	    }
+}
