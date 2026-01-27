95c17a8d0b030ba73100da28e82b9cdf0fa30a83
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-27 15:39:52 +0530

    [saurav] : Added longrunningtask program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/test/longrunningtask/PerformanceUtilsTest.java
@@ -0,0 +1,23 @@
+package com.junit.test.longrunningtask;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import com.junit.main.longrunningtask.PerformanceUtils;
+import java.util.concurrent.TimeUnit;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PerformanceUtilsTest {
+
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS) // Test will fail if method takes more than 2 seconds
+    public void testLongRunningTaskTimeout() {
+        PerformanceUtils.longRunningTask(); // This will fail because it sleeps 3 seconds
+    }
+
+    @Test
+    public void testLongRunningTaskNormal() {
+        String result = PerformanceUtils.longRunningTask();
+        assertEquals("Task Completed", result);
+    }
+}
+
