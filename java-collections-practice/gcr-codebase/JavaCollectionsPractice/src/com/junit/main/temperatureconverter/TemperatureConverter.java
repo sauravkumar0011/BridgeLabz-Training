7a0c5534203871d10adc06047cc4d7d00ce5afcf
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-27 15:33:45 +0530

    [saurav] : Added temperatureconverter program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/main/temperatureconverter/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package com.junit.main.temperatureconverter;
+
+public class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
+
