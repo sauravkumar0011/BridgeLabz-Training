2466b35eb17f320900b8e9070077aa8e05cc663f
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-30 22:37:28 +0530

    [saurav] : Added notificationfiltering program

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/lambdaexpression/notificationfiltering/Alert.java
@@ -0,0 +1,9 @@
+package com.lambdaexpressioon.notificationfiltering;
+
+public class Alert {
+	
+	String type;
+	Alert(String type){
+		this.type = type;
+	}
+}
---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/lambdaexpression/notificationfiltering/Test.java
@@ -0,0 +1,25 @@
+package com.lambdaexpressioon.notificationfiltering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		List<Alert> alerts = new ArrayList<>(List.of(new Alert("Emergency"),
+		         new Alert("Info"),
+		         new Alert("Medicine"),
+		         new Alert("Info"),
+		         new Alert("Medicine")));
+		
+		//Alerts only for emergency
+		alerts.stream().filter(a -> a.type.equals("Emergency"))
+		.forEach(a -> System.out.println(a.type));
+		
+		//Alerts only for Medicine 
+		alerts.stream().filter(a -> a.type.equals("Medicine"))
+		.forEach(a -> System.out.println(a.type));
+	}
+
+}
