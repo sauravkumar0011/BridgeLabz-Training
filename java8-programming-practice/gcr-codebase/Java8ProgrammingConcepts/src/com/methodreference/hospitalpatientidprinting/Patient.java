cf969ec5626c31e2c52b0c409a2c0d66417cd1e8
sauravkumar0011 <studentlife9341@gmail.com>
2026-01-30 22:38:32 +0530

    [saurav] : Added hospital patient id printing program

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/methodreference/hospitalpatientidprinting/Patient.java
@@ -0,0 +1,14 @@
+package com.methodreference.hospitalpatientidprinting;
+
+public class Patient {
+
+	int id;
+	String name;
+	
+	public Patient(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	void displayId(){System.out.println("Id: " + id);}
+}
---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/methodreference/hospitalpatientidprinting/Test.java
@@ -0,0 +1,18 @@
+package com.methodreference.hospitalpatientidprinting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		List<Patient> patients = new ArrayList<>(List.of(new Patient(101,"saurav"),
+		           new Patient(102,"vikash"),
+		           new Patient(102,"raghav"),
+		           new Patient(102,"Teja")));
+		
+		patients.forEach(Patient::displayId);
+		}
+
+}
